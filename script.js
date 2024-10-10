d65f2fe0b1b64954385bc7339758e36ba5c42420
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2024-10-10 22:09:53 +0500

    This is JavaScript ToDo List
    This is JavaScript ToDo List

---
+++ script.js
@@ -0,0 +1,58 @@
+function addItem(){
+    var input=document.getElementById('input')
+    var list=document.getElementById('list')
+    var liElement=document.createElement('li')
+    var editBtn='<button onclick=editItem(event)>Edit</button>'
+    var deleteBtn='<button onclick=delItem(event)>Delete</button>'
+
+    liElement.innerHTML='<span>'+input.value+'</span>'+editBtn+deleteBtn
+    list.append(liElement)
+    input.value=''
+}
+
+
+function deleteAllItem() {
+    var list=document.getElementById('list');
+    list.innerHTML = " "
+}
+
+function delItem(event){
+    var liElement=event.target.parentElement
+    liElement.remove()
+}
+
+var itemElement
+
+function editItem(event){
+    var inputElement=document.getElementById('input')
+    itemElement=event.target.previousSibling
+    inputElement.value=itemElement.innerHTML
+
+    var addBtn=document.getElementById('add-btn');
+    addBtn.className='hide'
+
+    var updateBtn=document.getElementById('update-btn');
+    updateBtn.className=''
+
+    var deleteBtn=document.getElementById('deleteAll-btn');
+    deleteBtn.className='hide'
+
+}
+
+function updateItem(){
+    var inputElement=document.getElementById('input')
+    itemElement.innerHTML=inputElement.value
+
+    var updateBtn=document.getElementById('update-btn');
+    updateBtn.className='hide'
+
+    var deleteBtn=document.getElementById('deleteAll-btn');
+    deleteBtn.className=' '
+
+    var addBtn=document.getElementById('add-btn');
+    addBtn.className=''
+
+
+    inputElement.value=''
+
+}
